618a69fbdb83d14d7169bb8519e3ee1b1ff481c3
Kipachu <arsik7473838@gmail.com>
2024-10-27 18:52:34 +0500

    Initial Commit

---
+++ kinda-contacter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kinda-contacter
+//
+//  Created by Arsen Kipachu on 10/27/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ kinda-contacterUITests/kinda_contacterUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  kinda_contacterUITestsLaunchTests.swift
+//  kinda-contacterUITests
+//
+//  Created by Arsen Kipachu on 10/27/24.
+//
+
+import XCTest
+
+final class kinda_contacterUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
